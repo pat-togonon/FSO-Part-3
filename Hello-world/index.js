faf95de7666f8c063ee9b4f9c1333db08339e4c9
Pat Togonon <pat.togonon@gmail.com>
2025-03-13 10:01:33 +0800

    Connect to mongoDB

--- Hello-world/index.js
+++ Hello-world/index.js
@@ -1,27 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const cors = require('cors');
+const Note = require('./models/note');
+const mongoose = require('mongoose');
 
-app.use(cors());
 app.use(express.static('dist'));
-
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-];
 
 const requestLogger = (request, response, next) => {
   console.log('Method: ',  request.method);
@@ -30,6 +13,9 @@ const requestLogger = (request, response, next) => {
   console.log('---');
   next() 
 };
+
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 app.use(requestLogger);
@@ -43,34 +29,32 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes);
+  Note.find({}).then(notes => {
+    response.json(notes);
+  });
 });
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id;
-  const note = notes.find(note => note.id === id);
   
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note);
-  } else {
-    response.status(404).end()
-  };
-
+  })
 });
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id;
-  notes = notes.filter(note => note.id !== id);
-
-  response.status(204).end()
+  Note.findByIdAndDelete(request.params.id)
+    .then(deletedNote => {
+      if (deletedNote) {
+        response.json({ message: 'Note successfully deleted' });
+      } else {
+        response.status(400).json({ error: 'Note has already been deleted from server'})
+      }
+    })
+    .catch(error => {
+      response.status(400).json({ error: "Can't delete the note right now. Please try again later" })
+    })
+  
 });
-
-const generateId = () => {
-  const maxId = notes.length > 0 
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0;
-  return String(maxId + 1);
-}
 
 app.post('/api/notes', (request, response) => {
   
@@ -78,22 +62,40 @@ app.post('/api/notes', (request, response) => {
 
   if (!body.content) {
     return response.status(400).json({
-      error: 'content missing'
+      error: 'Content missing. Please enter your note'
       });
   };
 
-  const note = {
+  const note = new Note ({
     content: body.content,
     important: body.important || false,
-    id: generateId()
-  }
+  });
 
-  notes = notes.concat(note);
+  note.save().then(savedNote => {
+    response.json(savedNote);
+  })
 
-  response.json(note);
 }); 
 
 app.use(unknownEndpoint);
+
+// closing mongoose when app is terminated / exited
+
+const closeMongoDatabase = () => {
+  console.log('Closing Mongoose connection...');
+  mongoose.connection.close()
+    .then(() => {
+      console.log('Mongoose connection closed');
+      process.exit(0);
+    })
+    .catch(error => {
+      console.error('Failed to close Mongoose: ', error);
+      process.exit(1);
+    })
+};
+
+process.on('SIGINT', closeMongoDatabase);
+process.on('SIGTERM', closeMongoDatabase);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
